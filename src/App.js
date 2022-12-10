214fe46471b1fe56b3bd3d132821eb4db9fd6b9c
TatsuKenshi <rockinbg@gmail.com>
2022-12-10 21:19:48 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import PrivateRoute from "./src/pages/PrivateRoute";
 
+function App() {
+  return (
+    <Router>
+      {/* <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route
+          path="checkout"
+          element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer /> */}
+    </Router>
+  );
+}
 export default App;
