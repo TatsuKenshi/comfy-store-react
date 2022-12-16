18511d32c0fc5a8d1e0e25b490f80b4f9241cd3d
TatsuKenshi <rockinbg@gmail.com>
2022-12-16 13:55:53 +0100

    home hero basic styling

--- src/components/home-hero/index.js
+++ src/components/home-hero/index.js
@@ -5,9 +5,9 @@ import heroBg2 from "../../img/hero-bcg-2.jpeg";
 
 const HomeHero = () => {
   return (
-    <section>
+    <section className="bg-white">
       <article>
-        <h1>
+        <h1 className="text-3xl">
           Design Your <br /> Comfort Zone
         </h1>
         <p>
@@ -18,11 +18,24 @@ const HomeHero = () => {
           praesentium sit ex facere rem totam, nostrum repudiandae asperiores
           reprehenderit veniam id saepe.
         </p>
-        <NavLink to="/products">Shop Now</NavLink>
+        <NavLink
+          to="/products"
+          className="p-1 rounded-md border border-solid border-stone-900 text-stone-900 bg-amber-100 hover:text-amber-100 hover:bg-stone-900"
+        >
+          Shop Now
+        </NavLink>
       </article>
       <article>
-        <img src={heroBg} alt="hero-main" />
-        <img src={heroBg2} alt="hero-accent" />
+        <img
+          src={heroBg}
+          alt="hero-main"
+          className="max-w-[600px] max-h-[400px] w-[600px]"
+        />
+        <img
+          src={heroBg2}
+          alt="hero-accent"
+          className="max-w-[200px] max-h-[150px] w-[200px]"
+        />
       </article>
     </section>
   );
