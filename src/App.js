cfb6f7a42eb55264468c0d04bef29b8859fdcde9
TatsuKenshi <rockinbg@gmail.com>
2022-12-22 23:54:56 +0100

    routing changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./src/pages/PrivateRoute";
+// import PrivateRoute from "./src/pages/PrivateRoute";
 import Navbar from "./components/navbar/index";
 import Sidebar from "./components/sidebar/index";
 import Footer from "./components/footer/index";
 import Home from "./pages/home";
 import Error from "./pages/error/index";
 import About from "./pages/about/index";
-import Checkout from "./pages/checkout";
+// import Checkout from "./pages/checkout";
 import Contact from "./pages/contact";
 import Cart from "./pages/cart";
 import Products from "./pages/products";
@@ -25,14 +25,14 @@ function App() {
         <Route path="products" element={<Products />} />
         <Route path="products/:id" element={<SingleProductPage />} />
         <Route path="contact" element={<Contact />} />
-        <Route
+        {/* <Route
           path="checkout"
           element={
             <PrivateRoute>
               <Checkout />
             </PrivateRoute>
           }
-        />
+        /> */}
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
