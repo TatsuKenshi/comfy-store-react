e2897f56777cc84cfa0467d4f96766ce762acab7
TatsuKenshi <rockinbg@gmail.com>
2023-01-10 21:59:37 +0100

    basic cart page setup - empty cart return and localStorage

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { useCartContext } from "../../context/cart-context/CartContext";
+import { Link } from "react-router-dom";
+import CartContent from "../../components/cart-content/index";
+import PageHero from "../../components/page-hero/index";
 
 const Cart = () => {
-  return <div>index</div>;
+  const { cart } = useCartContext();
+
+  if (cart.length < 1) {
+    return (
+      <section className="bg-white">
+        <PageHero title="Cart" />
+
+        <div className="text-center pt-64 pb-96">
+          <h2 className="text-3xl font-bold text-yellow-800">
+            Your cart is empty...
+          </h2>
+          <button className="mt-8">
+            <Link
+              to="/products"
+              className="p-2 rounded-md border border-solid border-stone-900 text-stone-900 bg-amber-100 hover:text-amber-100 hover:bg-stone-900"
+            >
+              Start Shopping
+            </Link>
+          </button>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="bg-white">
+      <PageHero title="Cart" />
+
+      <div>
+        <CartContent />
+      </div>
+    </section>
+  );
 };
 
 export default Cart;
