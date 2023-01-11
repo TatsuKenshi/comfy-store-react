d0b45009af71dcca904136cb31c1079aa4212b10
TatsuKenshi <rockinbg@gmail.com>
2023-01-11 22:15:21 +0100

    adds basic Auth0 setup - Auth0Provider wrapper with parameters

--- src/index.js
+++ src/index.js
@@ -6,16 +6,27 @@ import { CartProvider } from "./context/cart-context/CartContext";
 import { FilterProvider } from "./context/filter-context/FilterContext";
 import { ProductsProvider } from "./context/products-context/ProductsContext";
 import { UserProvider } from "./context/user-context/UserContext";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// REACT_APP_AUTH0_DOMAIN=dev-n7f-c1rx.us.auth0.com
+// REACT_APP_AUTH0_CLIENT_ID=gGSLUhA05o3OlyEyoXZQ06F9M6393Rzg
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider
+      domain="dev-n7f-c1rx.us.auth0.com"
+      clientId="gGSLUhA05o3OlyEyoXZQ06F9M6393Rzg"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
